277ccc8c3f3ad5d489cf4cac943993b610aa8ee9
Neha218 <nehaapawar21@gmail.com>
2018-07-02 11:58:15 +0530

    first commit

---
+++ set.py
@@ -0,0 +1,18 @@
+
+#set A
+A=set(['apple','orange','apple','pear','orange','banana'])
+print("Set A is : ")
+print(A)
+
+#set B
+B=set(['cherry','banana','orange','mango','pear'])
+print("\nSet  is : ")
+print(B)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#letter in A but not in B
+print("\nDifference : ",A-B)
+
+#letters in either A or B
+print("Union : ",A|B)
